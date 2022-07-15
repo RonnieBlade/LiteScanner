b5113a6b52713a172c024bef380ec26ff5c0d709
Ronnie Blade <litesound.music@gmail.com>
2022-07-15 13:56:54 +0300

    Added DB Schema to createTablesIfNotExist.php

--- Server/dbconnect.php
+++ Server/dbconnect.php
@@ -1,9 +1,9 @@
 <?php
 
-$db_host="";
-$db_username="";
-$db_password="";
-$db_name="";
+$db_host="litesound.com";
+$db_username="a0037607_testCreate";
+$db_password="3332210";
+$db_name="a0037607_testCreate";
 
 $db_connect = mysqli_connect($db_host, $db_username, $db_password, $db_name);
 // Check connection
